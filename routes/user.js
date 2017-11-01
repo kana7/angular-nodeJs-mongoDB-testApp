7a5864b31568fb3809a23f48e66ae37fb86489d3
quentin Fonck <quentin.fonck@gmail.com>
2017-11-01 21:37:32 +0100

    Adding a route procedure to authenticate a user.

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
+// create a user in the database
 router.post("/", function(req, res, next) {
   console.log(req.body);
   var user = new User({
@@ -24,6 +26,43 @@ router.post("/", function(req, res, next) {
       message: 'User created',
       obj: result
     });
+  });
+});
+
+// Login route procedure
+router.post('/sigin', function(req, res, next){
+  // fetch user from database
+  User.findOne({email: req.body.email}, function(err, user){
+    // basic unexpected error from server
+    if(err){
+      return res.status(500).json({
+        title: 'An error occured',
+        error: err
+      });
+    }
+    // is the email exist for a user?
+    if(!user){
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message: 'Invalid login credentials'}
+      });
+    }
+    // is the password correct?
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(401)
+        .json({
+          title: "Login failed",
+          error: { message: "Invalid login credentials" }
+        });
+    }
+    // create the JWT token after veryfying all login credentials
+    var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+    // send back to the front end the token and the user id.
+    res.status(200).json({
+      message: 'Successfully logged in',
+      token: token,
+      userId: user._id
+    })
   });
 });
 
